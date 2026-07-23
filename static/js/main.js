5bf2cbd6772c95d80f4fa0c69cb3126d947e063f
彼岸临窗 <115102@qq.com>
2026-07-23 12:41:46 +0800

    版本号更新

--- static/js/main.js
+++ static/js/main.js
@@ -1235,7 +1235,7 @@ window.addEventListener('load', function() {
 
 /**开源不易，请尊重作者的版权，保留本信息**/
 function showConsoleInfo() {
-    const version = '3.7.1';
+    const version = '3.7.2';
     const copyright = '自豪地使用OneBlog主题';
     console.log('\n' + ' %c 当前版本：' + version + '  ' + copyright + '  %c https://023.me  ' + '\n', 'color: #fadfa3; background: #030307; padding:5px 0;', 'background: #fadfa3; padding:5px 0;');
     console.log('开源不易，请尊重作者版权，保留基本的版权信息。');
